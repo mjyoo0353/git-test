95eb0b927d8e89f138aa397e3e821ac9fe03fd20
Minji Yoo <ryuminji0353@gmail.com>
2023-11-16 14:40:33 +0900

    commit test 4

--- src/main/java/org/galapagos/controller/HomeController.java
+++ src/main/java/org/galapagos/controller/HomeController.java
@@ -20,6 +20,9 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	//test
 	//test2
+	
+	
+	// test git stash
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
