86e0cd14c3572342bc516e2201bba18fbdbf6589
Minji Yoo <ryuminji0353@gmail.com>
2023-11-16 14:44:57 +0900

    commit test 6

--- src/main/java/org/galapagos/controller/HomeController.java
+++ src/main/java/org/galapagos/controller/HomeController.java
@@ -21,22 +21,16 @@ public class HomeController {
 	//test
 	//test2
 	
+	//test git stash
+
 	
-	// test git stash
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
 		String formattedDate = dateFormat.format(date);
-		
 		model.addAttribute("serverTime", formattedDate );
-		
 		return "home";
 	}
 	
